1312f0294af1e843aeac30192dbbe3c39991d7e4
Srinidhi Kashyap <skashyap@eigenrisk.com>
2019-01-28 15:30:31 +0530

    changed the server file to serve the right folder

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
+const path = require('path');
+const http = require('http');
+
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000);
+
 const app = express();
-const port = process.env.PORT || 5000;
 const apis = require('./apis');
+const dev = app.get('env') !== 'production';
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+if(!dev) {
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+    app.get('/getSummary', apis.getSummary);
+    app.get('/getConScores', apis.getConScores);
+    app.get('/getProScores', apis.getProScores);
+    app.get('/getProFrequencies', apis.getProFrequencies);
+    app.get('/getConFrequencies', apis.getConFrequencies);
+
+if(dev) {
+    app.use(morgan('dev'));
+}
 
 
-app.get('/', (req, res) => {
-    res.send('Home Page')
-});
+const server = http.createServer(app);
 
-app.get('/getSummary', apis.getSummary);
-app.get('/getConScores', apis.getConScores);
-app.get('/getProScores', apis.getProScores);
-app.get('/getProFrequencies', apis.getProFrequencies);
-app.get('/getConFrequencies', apis.getConFrequencies);
+server.listen(PORT, err => {
+    if(err) throw err;
+    console.log('Server Started!')
+})
